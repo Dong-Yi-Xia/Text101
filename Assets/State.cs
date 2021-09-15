9482a74cd3d1ac001346436fd5eaea53f8501323
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-14 20:01:29 -0400

    new array states

--- Assets/State.cs
+++ Assets/State.cs
@@ -9,9 +9,19 @@ public class State : ScriptableObject
   //create a text area field, field in unity, type, variable name
   [TextArea(10, 14)] [SerializeField] string storyText;
 
+  //create a State Array field called nextStates in Unity
+  [SerializeField] State[] nextStates;
+
+  //create public function type: string
   public string GetStateStory()
   {
     return storyText;
+  }
+
+  //create a public function type: State Array
+  public State[] GetNextStates()
+  {
+    return nextStates;
   }
 
 }
