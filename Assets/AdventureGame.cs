e3cfcc1fcc5c60f725363656b8fe527bd53730e3
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-18 19:18:03 -0400

    next state

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,13 @@ public class AdventureGame : MonoBehaviour
   [SerializeField] Text textComponent;
   [SerializeField] State startingState;
 
-  //call the public class State, then assign to a variable  
+  //type State, then assign to a variable  
   State theCurrentState;
 
   // Start is called before the first frame update
   void Start()
   {
-    //the theCurrentState will be the startingStateFool state file loaded in Unity
+    //the theCurrentState will be the startingState state file loaded in Unity, state file will have the method of GetStateStory
     theCurrentState = startingState;
     textComponent.text = theCurrentState.GetStateStory();
   }
@@ -23,6 +24,25 @@ public class AdventureGame : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    ManageState();
+  }
 
+  private void ManageState()
+  {
+    // type State[] is replaced with var. since we know that GetNextStates() return an array
+    var nextStates = theCurrentState.GetNextStates();
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      theCurrentState = nextStates[0];
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      theCurrentState = nextStates[1];
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha3))
+    {
+      theCurrentState = nextStates[3];
+    }
+    textComponent.text = theCurrentState.GetStateStory();
   }
 }
